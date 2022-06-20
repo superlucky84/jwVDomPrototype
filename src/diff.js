d57ce037b029dacd421aca1aab2b65acd307d8fd
김진우 <kim.jinwoo2@joongang.co.kr>
2022-06-20 15:36:07 +0900

    feat: 루프형 플레그먼트형 일반태그형 구분하여 가상돔을 다시 만들도록 코드 추가

--- src/diff.js
+++ src/diff.js
@@ -9,14 +9,23 @@
  * 2. 1번에 해당하지 않을 경우 props와 데이터만 일단 교체
  * 3. props와 data가 오리지날 가상돔과 얕은비교를 통해 완전히 같다면 children을 교체하지 않고 끝냄 (일단 구현 생략)
  * 4. props와 datark 오리지날 가상동과 얕은비교를 통해 틀리다면 children을 루프톨면서 배교해줌 (일단 구현생략)
+ * 5. loop 타입은 일단 항상 다시 그리고 나중에 개선 (일단 구현 생략)
  */
 
-function checkFragment(vDom) {
-  return typeof vDom === 'function' && tag.name === 'Fragment';
+function checkCustemComponent(vDom) {
+  return typeof vDom === 'function';
 }
 
-function checkCustemComponent(vDom) {
-  return typeof tag === 'function' && tag.name !== 'Fragment';
+function checkFragment(vDom) {
+  return vDom.type === 'fragment';
+}
+
+function checkTagElement(vDom) {
+  return vDom.type === 'element';
+}
+
+function checkLoopElement(vDom) {
+  return vDom.type === 'loop';
 }
 
 function checkSameCustomComponent(originalVdom, newVdom) {
@@ -30,41 +39,77 @@ function checkSameFragment({ originalVdom, newVdom }) {
   );
 }
 
-function checkSameElement(originalVdom, newVdom) {
-  return originalVdom.type === 'element' && newVdom.tag === originalVdom.tag;
+function checkSameTagElement({ originalVdom, newVdom }) {
+  originalVdom.type === 'element' && originalVdom.tag === newVdom.tag;
 }
 
-export default function vDomDiff({ originalVdom, newVdom }) {
+export default function makeNewVdomTree({ originalVdom, newVdom }) {
   const isComponent = checkCustemComponent(newVdom);
   const isFragment = checkFragment(newVdom);
+  const isTagElement = checkTagElement(newVdom);
+  const isLoopElement = checkLoopElement(newVdom);
 
   if (isComponent) {
     processingComponent({ originalVdom, newVdom });
   } else if (isFragment) {
     processingFragment({ originalVdom, newVdom });
+  } else if (isTagElement) {
+    processingTagElement({ originalVdom, newVdom });
+  } else if (isLoopElement) {
+    processingLoopElement({ originalVdom, newVdom });
+  }
+}
+
+/**
+ * 루프형은 우선 무조건 새로 만듬
+ * 추후 키값 있을때는 원본 엘리먼트를 유지하도록 개선 예정
+ */
+function processingLoopElement({ originalVdom, newVdom }) {
+  newVdom.children.forEach(item => {
+    makeNewVdomTree({ newVdom: item });
+  });
+}
+
+function processingTagElement({ originalVdom, newVdom }) {
+  const isSameTagElement = checkSameTagElement({ originalVdom, newVdom });
+
+  if (!originalVdom || !isSameTagElement) {
+    newVdom.children.forEach(item => {
+      makeNewVdomTree({ newVdom: item });
+    });
+  } else if (originalVdom && isSameTagElement) {
+    newVdom.children.forEach((item, index) => {
+      makeNewVdomTree({ newVdom: item, originalVdom: originalVdom[index] });
+    });
   }
 }
 
 function processingFragment({ originalVdom, newVdom }) {
   const isSameFragment = checkSameFragment({ originalVdom, newVdom });
 
-  console.log('ISSAMEFRAGMENT = ', isSameFragment);
+  if (!originalVdom || !isSameFragment) {
+    newVdom.children.forEach(item => {
+      makeNewVdomTree({ newVdom: item });
+    });
+  } else if (originalVdom && isSameFragment) {
+    newVdom.children.forEach((item, index) => {
+      makeNewVdomTree({ newVdom: item, originalVdom: originalVdom[index] });
+    });
+  }
 }
 
 function processingComponent({ originalVdom, newVdom }) {
-  const isRoot = renderCount === 0;
-  const isSameCustomComponent =
-    checkSameCustomComponent(originalVdom, newVdom) || isRoot;
+  const isSameCustomComponent = checkSameCustomComponent(originalVdom, newVdom);
 
   if (!originalVdom || !isSameCustomComponent) {
     newVdom = newVdom();
-    newVdom.children.forEach((item, index) => {
-      vDomDiff({ newVdom: item });
+    newVdom.children.forEach(item => {
+      makeNewVdomTree({ newVdom: item });
     });
-  } else if (isSameCustomComponent && originalVdom) {
+  } else if (originalVdom && isSameCustomComponent) {
     newVdom = newVdom(originalVdom.stateKey);
     newVdom.children.forEach((item, index) => {
-      vDomDiff({ newVdom: item, originalVdom: originalVdom[index] });
+      makeNewVdomTree({ newVdom: item, originalVdom: originalVdom[index] });
     });
   }
 }
