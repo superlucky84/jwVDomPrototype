4c040dc9d2ff05b865b4f1646c1ac81344b8a875
김진우 <kim.jinwoo2@joongang.co.kr>
2022-06-20 14:55:12 +0900

    주석 수정

--- src/diff.js
+++ src/diff.js
@@ -7,8 +7,8 @@
  *    1-2-1. 같다면 children을 루프톨면서 배교해줌
  *   v 1-3. 부모 엘리먼트의 태그이름이 다를때
  * 2. 1번에 해당하지 않을 경우 props와 데이터만 일단 교체
- * 3. props와 data가 오리지날 가상돔과 얕은비교를 통해 완전히 같다면 children을 교체하지 않고 끝냄
- * 4. props와 datark 오리지날 가상동과 얕은비교를 통해 틀리다면 children을 루프톨면서 배교해줌
+ * 3. props와 data가 오리지날 가상돔과 얕은비교를 통해 완전히 같다면 children을 교체하지 않고 끝냄 (일단 구현 생략)
+ * 4. props와 datark 오리지날 가상동과 얕은비교를 통해 틀리다면 children을 루프톨면서 배교해줌 (일단 구현생략)
  */
 
 function checkFragment(vDom) {
