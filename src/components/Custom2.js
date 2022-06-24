de9aebe83f9e9cc6cd89b137d9d430fe99649803
김진우 <kim.jinwoo2@joongang.co.kr>
2022-06-24 14:02:14 +0900

    feat: useState 테스트중 커밋

--- src/components/Custom2.js
+++ src/components/Custom2.js
@@ -1,11 +1,9 @@
 import { h, Fragment } from '../jsx';
 
 export default function Custom2({ props, children }) {
-  const j = 7;
-
   return (
     <div class="custom2">
-      {j} - a{props.k}
+      {props.k}
       <article>{children}</article>
     </div>
   );
--- src/components/CustomElement.js
+++ src/components/CustomElement.js
@@ -3,18 +3,23 @@ import Custom2 from './Custom2';
 
 export default function CustomElement({ props, children, useState }) {
   const [j, setJ] = useState(3);
+  const [w, setW] = useState(3);
   const handle = () => {
     setJ(j + 1);
+  };
+  const handle2 = () => {
+    setW(w + 1);
   };
 
   return (
     <div class={`aa${j}`} kk={j}>
       <span onClick={handle}>{j}-vava</span>
+      <span onClick={handle2}>{w}-vava</span>
       <Custom2 k={j} />
       <Custom2 k={j + 1} />
       <div>
-        <span>1</span>
-        <span>2</span>
+        <div>1{j}</div>
+        <div>2{w}</div>
       </div>
     </div>
   );
